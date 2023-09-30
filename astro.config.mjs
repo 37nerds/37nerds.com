1b0d35abbf00544c1f26b5957ad8a2e472b7d971
Shihab Mahamud <shihab4t@gmail.com>
2023-10-01 01:33:12 +0600

    upgrade to astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
-import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://37nerds.github.io",
     base: "/37nerds.com",
-    integrations: [
-        tailwind(),
-        image({
-            serviceEntryPoint: "@astrojs/image/sharp",
-        }),
-    ],
+    integrations: [tailwind()],
+    image: {
+        service: squooshImageService(),
+    },
 });
