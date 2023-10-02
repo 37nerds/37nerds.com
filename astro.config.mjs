d7efc02467b9104e7458f5a2e9af99ffbec5886f
Shihab Mahamud <shihab4t@gmail.com>
2023-10-02 07:28:16 +0600

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig, squooshImageService } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     site: "https://37nerds.github.io",
-    base: "/37nerds.com",
     integrations: [tailwind()],
     image: {
         service: squooshImageService(),
